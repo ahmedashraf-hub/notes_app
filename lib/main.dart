a09e3d30e151b62666c63cfa1cb105fd8cef6ee3
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-07-09 15:57:50 +0300

    Init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/eidt_note_view.dart';
 import 'package:notes_app/views/home_veiw.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(NotesAPP());
 }
 
--- lib/widgets/add_note_button_sheet.dart
+++ lib/widgets/add_note_button_sheet.dart
@@ -7,20 +7,36 @@ class AddNoteButtonSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SizedBox(height: 32),
-            CustomTextField(hint: 'title'),
-            SizedBox(height: 16),
-            CustomTextField(hint: 'Content', maxLines: 5),
-            SizedBox(height: 32),
-            CustomButton(),
-            SizedBox(height: 16),
-          ],
-        ),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: SingleChildScrollView(child: AddNoteForm()),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: const [
+          SizedBox(height: 32),
+          CustomTextField(hint: 'title'),
+          SizedBox(height: 16),
+          CustomTextField(hint: 'Content', maxLines: 5),
+          SizedBox(height: 32),
+          CustomButton(),
+          SizedBox(height: 16),
+        ],
       ),
     );
   }
